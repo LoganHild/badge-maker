533bcbe6ad503d8eedc58822053ec1d9735b60d2
Logan Hildebrandt <hildebrandtlogan@gmail.com>
2022-02-09 19:22:58 -0600

    added logic in Util file to create csv files

--- Program.cs
+++ Program.cs
@@ -1,41 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace CatWorx.BadgeMaker {
-  class Program {
-    static void Main(string[] args) 
-    {
-      List<Employee> employees = GetEmployees();
-      Util.PrintEmployees(employees);
-    }
-    static List<Employee> GetEmployees() {
+namespace CatWorx.BadgeMaker 
+  {
+    class Program 
+      {
+        static void Main(string[] args) 
+          {
+            List<Employee> employees = GetEmployees();
+            Util.PrintEmployees(employees);
+            Util.MakeCSV(employees);
+          }
+        static List<Employee> GetEmployees() 
+          {
 
-      List<Employee> employees = new List<Employee>();
-      while(true) {
-        // Move the initial prompt inside the loop, so it repeats for each employee
-        Console.WriteLine("Enter first name (leave empty to exit): ");
+            List<Employee> employees = new List<Employee>();
+            while(true) 
+              {
+                // Move the initial prompt inside the loop, so it repeats for each employee
+                Console.WriteLine("Enter first name (leave empty to exit): ");
 
-        // change input to firstName
-        string firstName = Console.ReadLine();
-        if (firstName == "") {
-          break;
-        }
+                // change input to firstName
+                string firstName = Console.ReadLine();
+                if (firstName == "") {
+                  break;
+                }
 
-        // add a Console.ReadLine() for each value
-        Console.Write("Enter last name: ");
-        string lastName = Console.ReadLine();
+                // add a Console.ReadLine() for each value
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine();
 
-        Console.Write("Enter ID: ");
-        int id = Int32.Parse(Console.ReadLine());
+                Console.Write("Enter ID: ");
+                int id = Int32.Parse(Console.ReadLine());
 
-        Console.Write("Enter Photo URL:");
-        string photoUrl = Console.ReadLine();
-        
-        Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
-        employees.Add(currentEmployee);
+                Console.Write("Enter Photo URL:");
+                string photoUrl = Console.ReadLine();
+                
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+              }
+
+            return employees;
+          }
       }
-
-      return employees;
-    }
   }
-}
